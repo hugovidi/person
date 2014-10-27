fa195f31fded2798ee78d154b06b65c416b1eb10
Hugo <hugo_vidinhas@hotmail.com>
2014-10-27 09:23:12 +0100

    proyecto person finalizado

---
+++ person.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class person
+{
+    private int edad;
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class person
+     */
+    public person(int age)
+    {
+        // initialise instance variables
+        edad = age;
+    }
+    
+    public int getAge()
+    {
+        return edad;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
